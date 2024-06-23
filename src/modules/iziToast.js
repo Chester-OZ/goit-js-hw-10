8591032770a302d124357a4208c95a879ceee4d7
Chester-OZ <153185988+Chester-OZ@users.noreply.github.com>
2024-06-23 12:29:38 +0300

    refactoring iziToast

--- src/modules/iziToast.js
+++ src/modules/iziToast.js
@@ -1,63 +1,45 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+const optionsFlags = {
+  icon: '',
+  messageColor: 'white',
+  timeout: 2000,
+  position: 'topRight',
+  close: false,
+  closeOnEscape: true,
+  closeOnClick: true,
+  displayMode: 'replace',
+};
+
 const errorFlag = () =>
   iziToast.error({
     title: 'Error',
     message: 'Please choose a date in the future',
-    icon: '',
-    messageColor: 'white',
-    timeout: 2000,
     backgroundColor: '#CB1E1E',
-    position: 'topRight',
-    close: false,
-    closeOnEscape: true,
-    closeOnClick: true,
-    displayMode: 'replace',
+    ...optionsFlags,
   });
 
 const successFlag = () =>
   iziToast.success({
     title: 'Success',
     message: 'Countdown finished!',
-    icon: '',
-    messageColor: 'white',
-    timeout: 2000,
     backgroundColor: '#1ECB75',
-    position: 'topRight',
-    close: false,
-    closeOnEscape: true,
-    closeOnClick: true,
-    displayMode: 'replace',
+    ...optionsFlags,
   });
 
 const errorFlagSnackbar = delay =>
   iziToast.error({
     message: `❌ Rejected promise in ${delay}ms`,
-    icon: '',
-    messageColor: 'white',
-    timeout: 2000,
     backgroundColor: '#CB1E1E',
-    position: 'topRight',
-    close: false,
-    closeOnEscape: true,
-    closeOnClick: true,
-    displayMode: 'replace',
+    ...optionsFlags,
   });
 
 const successFlagSnackbar = delay =>
   iziToast.success({
-    position: 'topRight',
     message: `✅ Fulfilled promise in ${delay}ms`,
-    icon: '',
-    messageColor: 'white',
-    timeout: 2000,
     backgroundColor: '#1ECB75',
-    position: 'topRight',
-    close: false,
-    closeOnEscape: true,
-    closeOnClick: true,
-    displayMode: 'replace',
+    ...optionsFlags,
   });
 
 export { errorFlag, successFlag, errorFlagSnackbar, successFlagSnackbar };
